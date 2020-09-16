378bcd46fac1af2eddc06a380bafb6209dcf3756
Yifei Xu <yifei21xu@gmail.com>
2020-09-15 22:46:51 -0700

    Development (#1)
    * Added features
    
    * Updated README
    
    * added configurable n-grams for tweetgen
    
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-44-62.us-east-2.compute.internal>

--- cogs/osu.py
+++ cogs/osu.py
@@ -8,19 +8,19 @@ from .utils import arg_parse
 
 load_dotenv()
 
-class Osu(commands.Cog):
+class Osu( commands.Cog ):
 	"""osu! cog"""
-	def __init__(self, bot):
+	def __init__( self, bot ):
 		self.bot = bot
-		self.api_key = os.getenv('OSU_TOKEN')
+		self.api_key = os.getenv( 'OSU_TOKEN' )
 		self.base_url = "https://osu.ppy.sh"
 
 
 	# ------------------- common osu functions ------------------- #
 
 	# Return dictionary containing mode name and id
-	def get_mode_id(self, osu_mode):
-		osu_mode = str(osu_mode)
+	def get_mode_id( self, osu_mode ):
+		osu_mode = str( osu_mode )
 
 		if osu_mode.lower() == "taiko" or osu_mode.lower() == "t" or osu_mode == "1":
 			return { "name": "osu!taiko", "id": 1 }
@@ -33,7 +33,7 @@ class Osu(commands.Cog):
 
 
 	# Return mod name from mod id
-	def get_mod_name(self, mod_id):
+	def get_mod_name( self, mod_id ):
 		mods = {
 			"0": "NM",
 			"1": "NF",
@@ -74,83 +74,84 @@ class Osu(commands.Cog):
 			return "MOD NOT FOUND"
 
 	# Fetch get_user from osu! api v1
-	def get_user_info(self, osu_user_id, osu_mode_id=0):
-		return requests.get(self.base_url + "/api/get_user", params={
+	def get_user_info( self, osu_user_id, osu_mode_id=0 ):
+		return requests.get( self.base_url + "/api/get_user", params={
 			'k': self.api_key, 
 			'u': osu_user_id, 
 			'm': osu_mode_id,
-			'type': 'string'}).json()[0]
+			'type': 'string'} ).json()[0]
 
 
 	# Fetch get_user_best from osu! api v1
-	def get_user_best_info(self, osu_user_id, osu_mode_id=0, osu_limit=100):
-		return requests.get(self.base_url + "/api/get_user_best", params={
+	def get_user_best_info( self, osu_user_id, osu_mode_id=0, osu_limit=100 ):
+		return requests.get( self.base_url + "/api/get_user_best", params={
 			'k': self.api_key, 
 			'u': osu_user_id, 
 			'm': osu_mode_id,
 			'limit': osu_limit,
-			'type': 'string'}).json()
+			'type': 'string'} ).json()
 
 
 	# Fetch get_beatmaps from osu! api v1
-	def get_beatmap_info(self, diff_id):
-		return requests.get(self.base_url + "/api/get_user_best", params={
+	def get_beatmap_info( self, diff_id ):
+		return requests.get( self.base_url + "/api/get_user_best", params={
 			'k': self.api_key, 
 			'u': osu_user_id, 
 			'm': osu_mode_id,
 			'limit': osu_limit,
-			'type': 'string'}).json()
+			'type': 'string'} ).json()
+
 
 	# ------------------- osu! discord commands ------------------- #
 
-	@commands.command(name="osu")
-	async def _get_osu_user(self, ctx):
+	@commands.command( name="osu" )
+	async def _get_osu_user( self, ctx ):
 		""" Output embed with basic osu player stats """
-		args = arg_parse.parse(ctx.message.content)
-		if len(args) == 0:
+		args = arg_parse.parse( ctx.message.content )
+		if len( args ) == 0:
 			await ctx.send("No user was provided")
 			return
 
 		try:
 			osu_user_id = args[0]
-			osu_mode = self.get_mode_id(args[1])
+			osu_mode = self.get_mode_id( args[1] )
 		except:
 			osu_user_id = args[0]
-			osu_mode = self.get_mode_id(0)
+			osu_mode = self.get_mode_id( 0 )
 
-		r = self.get_user_info(osu_user_id, osu_mode['id'])
+		r = self.get_user_info( osu_user_id, osu_mode['id'] )
 
 		embed = discord.Embed(
 			title = ":flag_{}:  {}  (#{})  ({}#{})".format(r['country'].lower(), r['username'], r['pp_rank'], r['country'], r['pp_country_rank']), 
 			url = self.base_url + "/u/" + r['user_id'],
-			colour = discord.Colour(0xfcba03),
+			colour = discord.Colour( 0xfcba03 ),
 			description = """**Total PP:**  {}
 **Accuracy:**  {}%
 **Playcount:**  {}
 **Join date:**  {}
-""".format(r['pp_raw'], "{:.2f}".format(float(r['accuracy'])), r['playcount'], r['join_date'])
+""".format( r['pp_raw'], "{:.2f}".format( float( r['accuracy'] ) ), r['playcount'], r['join_date'])
 		)
-		embed.set_thumbnail(url = "https://a.ppy.sh/{}".format(r['user_id']))
-		embed.set_footer(text = "{} profile for {}".format(osu_mode['name'], r['username']))
+		embed.set_thumbnail( url = "https://a.ppy.sh/{}".format( r['user_id'] ) )
+		embed.set_footer( text = "{} profile for {}".format( osu_mode['name'], r['username'] ) )
 
-		await ctx.send(embed = embed)
+		await ctx.send( embed = embed )
 
 
-	@commands.command(name="onemiss")
-	async def _get_onemiss(self, ctx):
+	@commands.command( name="onemiss" )
+	async def _get_onemiss( self, ctx ):
 		""" Output 1x miss statistics """
-		args = arg_parse.parse(ctx.message.content)
-		if len(args) == 0:
-			await ctx.send("No user was provided")
+		args = arg_parse.parse( ctx.message.content )
+		if len( args ) == 0:
+			await ctx.send( "No user was provided" )
 			return
 
 		osu_user_id = args[0]
 
-		top_plays = self.get_user_best_info(osu_user_id, osu_user_id)
+		top_plays = self.get_user_best_info( osu_user_id, osu_user_id )
 		onemiss_plays = [play for play in top_plays if play['countmiss'] == '1']
-		onemiss_cnt = len(onemiss_plays)
+		onemiss_cnt = len( onemiss_plays )
 
-		await ctx.send("{} has {} 1x misses in their top 100 plays!".format(osu_user_id, onemiss_cnt))
+		await ctx.send( "{} has {} 1x misses in their top 100 plays!".format( osu_user_id, onemiss_cnt ) )
 	
-def setup(bot):
-	bot.add_cog(Osu(bot))
+def setup( bot ):
+	bot.add_cog( Osu( bot ) )
